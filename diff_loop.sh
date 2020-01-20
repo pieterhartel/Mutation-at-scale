fa1e2f0a5a16369847a0a2120f3f260f9ec66b71
Pieter Hartel <pieter.hartel@tudelft.nl>
2020-01-20 17:44:54 +0100

    readme

--- diff_loop.sh
+++ diff_loop.sh
@@ -5,45 +5,37 @@ Tmp=/tmp/Diff_$$
 /bin/rm -rf /tmp/Diff_*
 mkdir -p $Tmp
 
-for Dir
-in *.dir
+for SubDir
+in *dir
 do
 	(
-		echo "+ cd $Dir"
-		cd $Dir
-		for SubDir
-		in *dir
+		echo "+ cd $SubDir"
+		cd $SubDir
+		Base=`echo $SubDir | sed -e 's/\(.*\)_......\.dir/\1/'`
+		for Mutant
+		in $Base.sol_*.log
 		do
-			(
-				echo "+ cd $SubDir"
-				cd $SubDir
-				Base=`echo $SubDir | sed -e 's/\(.*\)_......\.dir/\1/'`
-				for Mutant
-				in $Base.sol_*.log
-				do
-					if [ -f $Mutant ]
-					then
-						$Truffle/diff.sh $Tmp $Base.sol_0.log $Mutant
-						(
-							echo "Mutation:"
-							head -5 $Mutant
-							echo ""
-							echo "Killed:"
-							wc -l $Tmp/$Mutant.*.diff
-							echo ""
-							for Diff
-							in $Tmp/$Mutant.*.diff
-							do
-								if [ -s $Diff ]
-								then
-									echo "$Diff"
-									cat $Diff
-								fi
-							done
-						) >$Mutant.diff
-					fi
-				done
-			)
+			if [ -f $Mutant ]
+			then
+				bash $Truffle/diff.sh $Tmp $Base.sol_0.log $Mutant
+				(
+					echo "Mutation:"
+					head -5 $Mutant
+					echo ""
+					echo "Killed:"
+					wc -l $Tmp/$Mutant.*.diff
+					echo ""
+					for Diff
+					in $Tmp/$Mutant.*.diff
+					do
+						if [ -s $Diff ]
+						then
+							echo "$Diff"
+							cat $Diff
+						fi
+					done
+				) >$Mutant.diff
+			fi
 		done
 	)
 done
